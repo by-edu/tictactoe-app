4f9bff73f79081fe66c1be9577c5350f4820bf55
Luciel <55259900+vLuciel@users.noreply.github.com>
2022-06-23 22:41:34 -0600

    1.0.5
    MainActivity.kt:
    
    + Written to use intent and open the new instance when pressing the play button
    + Written to take values given from the edited name, symbol select, and turn select
    
    \ Not certain if entirely functional; does not properly lead to new instance when button is pressed currently
    
    activity_main.xml:
    
    \ No changes, previous push was aimed at activity_main.xml, not MainActivity.kt
    
    activity_tictactoe.xml: 
    
    + Added small placeholder text to display user name and symbol

--- MainActivity.kt
+++ MainActivity.kt
@@ -13,6 +13,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
 import android.widget.RadioGroup
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -22,12 +23,32 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.activity_main)
 
         // TODO (suggested): get a reference to the "play" button and use it to set its "onClick" listener to MainActivity
+        val btnPlay = findViewById<Button>(R.id.btnPlay)
+        btnPlay.setOnClickListener {
 
+            // need to actually use onClick
+
+        }
     }
 
     // TODO (suggested): get the player's name, symbol, and the first move choice; pass info to the TicTacToe activity
     override fun onClick(p0: View?) {
 
+        val editName = findViewById<EditText>(R.id.edtName)
+        // This should link to the name edit
+
+        val rgSymbolSelect: RadioGroup = findViewById(R.id.radioSymbol)
+        val rgTurn: RadioGroup = findViewById(R.id.radioTurn)
+        // These should link to each radio group
+
+        val rbChosenSymbol : RadioButton = findViewById(rgSymbolSelect.checkedRadioButtonId)
+        // id should be either 'radioNoughts' or 'radioCrosses'
+        val rbChosenTurn : RadioButton = findViewById(rgTurn.checkedRadioButtonId)
+        // id should be either 'buttonYes' (which is first) or 'buttonNo' (which is last)
+
+        val intent = Intent(this, TicTacToeActivity::class.java)
+        startActivity(intent)
+        // This should start the tictactoe class i believe
     }
 
 
