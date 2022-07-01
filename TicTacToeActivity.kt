b05c8ed13fdba606c2e84830ea844950edb1e706
Luciel <55259900+vLuciel@users.noreply.github.com>
2022-06-30 21:03:07 -0600

    1.9.0
    + State save is functional
    + Computer takes turn depending on setting, it's just written oddly
    
    \ Still need to add AI

--- TicTacToeActivity.kt
+++ TicTacToeActivity.kt
@@ -8,63 +8,125 @@ package edu.msudenver.tictactoe
  */
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import java.io.Serializable
 
-class TicTacToeActivity : AppCompatActivity(),View.OnClickListener {
+class TicTacToeActivity : AppCompatActivity(),View.OnClickListener, Serializable {
 
-    // TODO (suggested): maintain a reference to a TicTacToe object
+    // TODOd (suggested): maintain a reference to a TicTacToe object
 
-    val tictactoe = TicTacToe
-    // Maybe try val tictactoe = null
-    // we don't entirely know what to classify tictactoe as at first
-    // tictactoe should be an instance variable that other classes can maybe access
-    // val tictactoe : TicTacToe? = null
+    var tictactoe : TicTacToe? = null
+    //private var tttactivity : TicTacToeActivity?  = null
+
+    private var buttons = arrayOf<Array<Button?>>(arrayOfNulls<Button>(3), arrayOfNulls<Button>(3), arrayOfNulls<Button>(3))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tictactoe)
 
-        // TODO (suggested): get the player's name, symbol, and the first move choice from the activity's intent; use them to instantiate a TicTacToe game; decide whether the computer should play based on the first move choice
+        // TODOd (suggested): get the player's name, symbol, and the first move choice from the activity's intent; use them to instantiate a TicTacToe game; decide whether the computer should play based on the first move choice
         val grabbedName = intent.getStringExtra("name")
-        val playerSymbol = intent.getCharExtra("symbol", '.')
+        val playerSymbol = intent.getCharExtra("symbol", ' ')
         val playerTurn = intent.getBooleanExtra("turn", false)
             // playerturn works fine!
-        // i think here you save the reference to tictactoe, like tictactoe = -grabbed info-
-        // TODO (suggested): get a reference to the TextView "player info" area; update the TextView with the player's name and symbol
+
+        tictactoe = TicTacToe(grabbedName.toString(), playerSymbol)
+
+        // TODOd (suggested): get a reference to the TextView "player info" area; update the TextView with the player's name and symbol
         val playerInfo : TextView = findViewById(R.id.playerInfo)
-
-
         playerInfo.setText("Player $grabbedName is using $playerSymbol")
 
         // TODO (suggested): using a loop and button tags, update their texts and "onClick" listeners to TicTacToeActivity; remember to disable the button if it corresponds to a computer's first move
         // hint: use "findViewWithTag"
 
+        for (i in 0..2 ) {
+            for(j in 0..2){
+                var btnName : String = "btn$i$j"
+                var btnID : Int = resources.getIdentifier(btnName, "id", packageName)
+
+                buttons[i][j] = findViewById(btnID)
+                buttons[i][j]?.setOnClickListener(this)
+
+
+                // oh, this is gonna fry your computer maybe lol
+                // https://stackoverflow.com/questions/60899066/android-studio-kotlin-two-dimensional-button-array-got-java-lang-arrayindexoutof
+                // PLEEEEEASE look at this link
+
+            }
+        }
+
+        if (!playerTurn){
+            tictactoe?.computerPlay()
+            val split = tictactoe?.lastMove
+
+
+            buttons[split?.first!!][split?.second!!]!!.isEnabled = false
+            buttons[split?.first!!][split?.second!!]!!.text = tictactoe?.getComputerSymbol().toString()
+
+        }
+
     }
 
-    // TODO (suggested): display a Toast with a text based on the game's result
+    // TODOd (suggested): display a Toast with a text based on the game's result
     fun showResults() {
-        // can call get winner from tictactoe and check the player and computer's symbol
+
+        val result = if (tictactoe?.getWinner() == tictactoe?.playerSymbol){
+            Toast.makeText(this, "You won; congratulations!!!", Toast.LENGTH_SHORT).show()
+        } else if (tictactoe?.getWinner() == tictactoe?.getComputerSymbol()) {
+            Toast.makeText(this, "I won; computers are superior", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "Tie!!!", Toast.LENGTH_SHORT).show()
+        }
     }
 
     // TODO (suggested): cast the given view as a Button; disable the button so you don't forget; get the button's tag and use it to infer the player's move coordinates; make the move and update the button's text with the player's symbol; if the game is over, show results; otherwise, have the computer play; use TitTacToe's last move and "findViewWithTag" to get a reference to the button of the computer's play; disable the button so you don't forget; update the button's text with the computer's symbol; if the game is over, show results
     override fun onClick(view: View?) {
-        // this method is used whenever a button is clicked, make sure buttons disabled after button press
-        // you can see what button is pressed by getting access to the view, just like in MainActivity with the radiobuttons i think
-        // check for each turn and if game is over after each move!!!!!
-    }
+
+        var btnName : String = resources.getResourceName(view!!.id)
+        var btnLoc : String = btnName.substring(btnName.lastIndexOf("n") + 1)
+        var i: Int = btnLoc.get(0).toString().toInt()
+        var j: Int = btnLoc.get(1).toString().toInt()
 
 
-    // TODO: save the TicTacToe object using the outState Bundle
+        (view as Button)
+
+        if (view.text != null){
+            view.isEnabled = false
+        }
+
+        if (tictactoe?.playerPlay(i, j) == true){
+            view.isEnabled = false
+            view.text = tictactoe?.playerSymbol.toString()
+        }
+
+        if (tictactoe?.isGameOver() == true){
+            showResults()
+            } else {
+                tictactoe?.computerPlay()
+                buttons[tictactoe?.lastMove?.first!!][tictactoe?.lastMove?.second!!]!!.isEnabled = false
+                buttons[tictactoe?.lastMove?.first!!][tictactoe?.lastMove?.second!!]!!.text = tictactoe?.getComputerSymbol().toString()
+            }
+
+        if (tictactoe?.isGameOver() == true){
+            showResults()
+        }
+
+        }
+
+
+
+    // TODOd: save the TicTacToe object using the outState Bundle
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
+        outState.putSerializable("tictactoe", tictactoe)
     }
 
-    // TODO: restore the TicTacToe object using the savedInstanceState Bundle
+    // TODOd: restore the TicTacToe object using the savedInstanceState Bundle
     override fun onRestoreInstanceState(savedIntanceState: Bundle) {
         super.onRestoreInstanceState(savedIntanceState)
+        tictactoe = savedIntanceState.getSerializable("tictactoe") as TicTacToe?
     }
 }
 
